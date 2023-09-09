f222d1abb0599c4c5a94c2f77388a95ca328c541
Thanuja <thanuja@thanuja.local>
2023-09-09 14:14:47 +0530

    Initial commit.

---
+++ src/main/java/org/wso2/samples/is/easymeet/EasymeetApplication.java
@@ -0,0 +1,13 @@
+package org.wso2.samples.is.easymeet;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EasymeetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EasymeetApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/wso2/samples/is/easymeet/EasymeetApplicationTests.java
@@ -0,0 +1,13 @@
+package org.wso2.samples.is.easymeet;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EasymeetApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
